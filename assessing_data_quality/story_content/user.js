9e594bef183f50f80255f013b67a74b847fa7a07
rozeliavanzyl-max <rozeliavanzyl@gmail.com>
2026-01-05 13:31:35 +0200

    Add files via upload

---
+++ assessing_data_quality/story_content/user.js
@@ -0,0 +1,25 @@
+function ExecuteScript(strId)
+{
+  switch (strId)
+  {
+      case "5wDPV60Lmgp":
+        Script1();
+        break;
+      case "6W0fBGdErHz":
+        Script2();
+        break;
+  }
+}
+
+function Script1()
+{
+  started();
+}
+
+function Script2()
+{
+  var player = GetPlayer();
+
+completed(-1);
+}
+
